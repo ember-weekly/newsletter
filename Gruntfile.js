0c830f1b0c476499c8bc828f16cb7a2e78ace93a
Owain Williams <owain@Owains-MacBook-Air.local>
2015-11-23 20:48:05 -0500

    issue 135

--- Gruntfile.js
+++ Gruntfile.js
@@ -11,7 +11,7 @@ module.exports = function (grunt) {
 
     ////////////////////////////////////////////////////////////////////////
 
-    var newsletterYaml = newsletterDir + 'ew-issue-z133-[2015-11-08].yaml';
+    var newsletterYaml = newsletterDir + 'ew-issue-z135-[2015-11-22].yaml';
 
     ////////////////////////////////////////////////////////////////////////
 
